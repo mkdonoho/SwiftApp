a05e22e9a8a03e708231ef66e81cbe6345652ec8
Madison Donoho <maddie.donoho@gmail.com>
2017-07-30 13:18:28 -0400

    Commit #1

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var label1: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func coolButtonClick(sender: AnyObject)
+    {
+        label1.text = "Buttons are cool...."
+        tapCount += 1
+        
+        print(tapCount)
+        
+        if(tapCount >= 10)
+        {
+            label1.text = "Stop pressing the button!"
+        }
+        
+    }
+    @IBAction func buttonClick(sender: AnyObject)
+    {
+        label1.text = "TOUCH ME AGAINNNNNN"
+        print("Button tapped")
+    }//end buttonClick
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.lightGrayColor()
+        label1.text = "Fuck the police!"
+        label1.textColor = UIColor.whiteColor()
+        
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
-}
+}//end class
 
