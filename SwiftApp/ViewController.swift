447f365a18154cc3e39dd144b8aaf0e22af855c2
Madison Donoho <Maddie@madisons-macbook-pro.local>
2017-07-30 12:05:30 -0400

    Initial Commit

---
+++ SwiftApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by Madison Donoho on 7/30/17.
+//  Copyright © 2017 Madison Donoho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
